e552d5ab5fec80b6ff21ec5b9c1703546f70611e
mayunfei <mayunfei@mininglamp.com>
2020-11-19 17:37:21 +0800

    alter comment

--- src/main/java/com/airwallex/service/CalculatorService.java
+++ src/main/java/com/airwallex/service/CalculatorService.java
@@ -40,35 +40,35 @@ public class CalculatorService {
 
 
 
-    // 记录当前可以操作的数的栈
+    // A stack that records the number currently available for operation
     private Stack<Double> numbers = new Stack<>();
-    // 记录栈数据的操作日志
+    // Operation log recording stack data
     private Stack<List<Double>> logList = new Stack<>();
 
     public void doCalculator(String expression) throws NumTransException {
-        // 通过空格分隔切分输入的表达式
+        // Split the input expression by a space
         String[] paramArr = expression.split(" ");
         int paramArrLength = paramArr.length;
         List<String> paramList= Arrays.asList(paramArr);
         List<String> subList=new ArrayList<>();
         for (int i = 0; i < paramArrLength; i++) {
             String operator = paramArr[i];
-            // 判断是数字则入栈，记录栈日志
+            // If it is a number, it will be put on the stack and the stack log will be recorded
             if (CommonUtils.isNumber(operator)) {
                 numbers.push(Double.valueOf(operator));
                 CommonUtils.addLogList(numbers, logList);
                 continue;
             }
-            //正确检查
+            //Correct check
             boolean ck = OperatorCheck.check(operator,i,numbers);
             if(!ck){
                 subList = paramList.subList(i,paramList.size());
                 break;
             }
-            //枚举选择
+            //Enumeration selection
             OperatorSelector.selector(numbers,logList,operator);
         }
-        //打印日志
+        //print logs
         CommonUtils.printStack(numbers, subList);
     }
 
